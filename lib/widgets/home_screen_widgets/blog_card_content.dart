f950963362990ca02a7926a993239d50b6643e1b
Adarsh Verma <adarshverma0603@gmail.com>
2020-08-30 17:04:16 +0530

    Added gradient to blogcards

--- lib/widgets/home_screen_widgets/blog_card_content.dart
+++ lib/widgets/home_screen_widgets/blog_card_content.dart
@@ -11,53 +11,55 @@ class BlogCardContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: 5,
-      child: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => BlogScreen(blog)),
-          );
-        },
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(kRoundedRadius)),
-          margin: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-          width: MediaQuery.of(context).size.width * 0.66,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  Utils.trimString(blog.blogTitle, 150),
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        blog.blogTopic.toUpperCase(),
-                        style: kBlogTopicTextStyle,
-                      ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => BlogScreen(blog)),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.center,
+                colors: [Colors.black, Colors.transparent]),
+            borderRadius: BorderRadius.all(kRoundedRadius)),
+//        margin: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                Utils.trimString(blog.blogTitle, 150),
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      blog.blogTopic.toUpperCase(),
+                      style: kBlogTopicTextStyle,
                     ),
-                    Text(
-                      DateFormat('MMM dd')
-                          .format(DateTime.parse(blog.blogDate)),
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 12,
-                      ),
+                  ),
+                  Text(
+                    DateFormat('MMM dd').format(DateTime.parse(blog.blogDate)),
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 12,
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
